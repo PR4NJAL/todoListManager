9689ac553732f7022ba21b4a5582280bc7d12895
PR4NJAL <pranjalbhardwaj.in@gmail.com>
2025-09-07 17:11:59 +0530

    Working with files

--- main.cpp
+++ main.cpp
@@ -1,15 +1,34 @@
 #include <bits/stdc++.h> //Replace this with specfics later
 using namespace std;
 
+static int uid = 0; // global variable
+
 class taskList {
+private:
+  unordered_map<int, pair<string, bool>> map;
+
 public:
-  unordered_map<int, string> map;
+  void bootstrapping() {
+
+  } // meant to read from storage and set the hash map, on a seprate thread
 
   void addTask() {
     cout << "Please type in task -> ";
     string content;
     getline(cin, content);
-    map[0] = content;
+
+    ofstream file;
+    file.open("./content/" + to_string(map.size()) +
+              ".txt"); // Have to have content dir prior
+    if (!file.is_open())
+      cerr << "Error: Unable to open file";
+    else
+      file << content;
+
+    file.close();
+
+    map[map.size()].first = content;
+    map[map.size()].second = false;
     cout << endl << "Succesfully added the new task" << endl << endl;
   }
 
@@ -20,7 +39,9 @@ public:
     }
 
     for (const auto &m : map) {
-      cout << "UID:" << m.first << endl << "Content: " << m.second << endl;
+      cout << "UID:" << m.first << endl
+           << "Content: " << m.second.first << endl
+           << "Completed: " << m.second.second << endl;
     }
   }
 
@@ -29,9 +50,23 @@ public:
     cout << endl << "Please choose the uid of the task to be deleted -> ";
     int uid;
     cin >> uid;
-    string content = map[uid];
+    string content = map[uid].first;
     map.erase(uid);
     cout << endl << "Succesfully deleted task: " << endl << content << endl;
+  }
+
+  void editTask() {
+    // NOTE:Support for only content edits right now!!!!!
+    printTasks();
+    cout << endl << "Please choose the uid of the task to be edited -> ";
+    int uid;
+    cin >> uid;
+    cout << "Please type in new task content -> ";
+    string content;
+    getline(cin, content);
+    map[uid].first = content;
+    map[uid].second = false;
+    cout << endl << "Succesfully edited task: " << endl << content << endl;
   }
 };
 
@@ -67,9 +102,7 @@ int main() {
       break;
     case 4:
       cout << endl << "You chose to edit a task" << endl << endl;
-      // Choose uid
-      // Choose whether to edit the content
-      // or edit the completed boolean
+      newTaskList.editTask();
       break;
     default:
       cout << endl
